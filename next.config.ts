2d7cd1a78e0272a6b7a02c0e53691f19b982b09b
Pratchaya <pratchaya.tanapibulphol@gmail.com>
2025-07-18 13:20:51 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import { output } from "framer-motion/client";
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
+const nextConfig: NextConfig = {
+//   output: 'export',
+//   basePath: '', // กำหนด basePath ให้เป็นชื่อโฟลเดอร์ที่คุณวางไฟล์
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true, // ปิดการใช้งาน Next.js Image Optimization
+//   },
 };
 
 export default nextConfig;
